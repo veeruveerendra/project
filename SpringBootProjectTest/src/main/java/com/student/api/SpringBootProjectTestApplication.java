7b740ad52fa12b4fbadd88d466f03e0bada71da5
veeruveerendra <veerendra.surath034@gmail.com>
2022-03-04 16:36:37 +0530

    first commit

---
+++ SpringBootProjectTest/src/main/java/com/student/api/SpringBootProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.student.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectTestApplication.class, args);
+	}
+
+}
